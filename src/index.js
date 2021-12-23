9a05733622aa96813997e3c40cad4a6d21ba36f9
diegodevs <diegom.develops@gmail.com>
2021-12-23 16:51:34 -0400

    [ADD] documentation

--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
-/**
- * This function prints a hastag stair of n numbers of steps, depending the number that we pass it.
- * @param {int} n Numbers of steps in the stair.
- */
-function stairCase(n) {
-  try {
-    if (n < 0) throw new Error('N cannot be a negative number');
-    if (!n) throw new Error('Please, enter a correct number');
-    let array = [];
-    for (let i = 0; i <= n - 1; i++) {
-      array.push(" ");
-    }
-    for (let j = 1; j <= n; j++) {
-      array[array.length - j] = "#"
-      console.log(array.join(""));
-    }
-    return array.length;
-  } catch (error) {
-    console.error(error.message);
-  }
-}
-
-module.exports = stairCase;
+const stairCase = require('./utils/stairCase');
+// Remember to pass the number of rungs before executing: "N = {your number} npm run start" :)
+console.log('Enjoy your stair: ');
+console.log('-'.repeat(process.env.N))
+stairCase(process.env.N);
--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 const assert = require('chai').assert;
-const stairCase = require('../src');
+const stairCase = require('../src/utils/stairCase');
 
 describe('stairCase', function () {
-  it('Should print a stair of #, its steps depend of the number that we pass it', function () {
+  it('Should print a stair of #, its rungs depend of the number that we pass it', function () {
     assert.equal(stairCase(5), 5);
   });
 });
